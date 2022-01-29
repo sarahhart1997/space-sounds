5395e11acb54fc1c84bb8429d39d2cb6993b8693
ajmarrocco <ajmarrocco@gmail.com>
2022-01-28 21:16:14 -0500

    add img to index

--- develop/js/script.js
+++ develop/js/script.js
@@ -2,3 +2,25 @@
 var arr = ["Saturn","Juno","Kepler","Atmosphere","Ganymede","Titan","Radio","Echo","Rover"];
 
 console.log(arr[4]);
+
+var getPhoto = function() {
+    // format the github api url
+    var apiUrl = `https://images-api.nasa.gov/search?q=${arr[4]}`;
+
+    // make a get request to url
+    fetch(apiUrl).then(function(response) {
+    // request was successful
+        if (response.ok) {
+            response.json().then(function(data) {
+            // displayIssues(data);
+                var imagine = document.querySelector("footer");
+                imagine.src = data.collection.items[1].links[0].href;
+            });
+        } else {
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
+        }  
+    });
+};
+
+getPhoto();
