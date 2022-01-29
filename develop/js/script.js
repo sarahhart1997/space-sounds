753ef59f43a38a8cca172864bb89ebdcc2a1fe62
ajmarrocco <ajmarrocco@gmail.com>
2022-01-28 21:56:16 -0500

    add youtube api

--- develop/js/script.js
+++ develop/js/script.js
@@ -15,7 +15,7 @@ var myArr = arr[8]
 //TO BE ERASED: Console logs index string of array
 console.log(myArr);
 console.log(arr.length);
-//get Photo function
+//get Photo function (will probably require parameter)
 var getPhoto = function() {
     // format the nasa image api url
     var apiUrl = `https://images-api.nasa.gov/search?q=${myArr}`;
@@ -52,10 +52,42 @@ sounds = [
 ]
 //TO BE ERASED: Calls index in array (replace with button call)
 var mySound = sounds[8]
-//
+//saving to Storage function (probably will require parameter)
 var savingToStorage = function(){
     localStorage.setItem(myArr, mySound);
 }
 //calls saving Storage function
 savingToStorage();
+//gets youtube video
+var getYt = function() {
+    // format the github api url
+    // var apiUrl = "https://youtube.googleapis.com/youtube/v3/search?channelId=UCvp2nA5u8vapl0iC_qDH2cA&key=AIzaSyDZ9fGPHgNx9yGoHa1Xc2oi-Xl5sIP1Umc";
+    var apiUrl = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=nasa&key=AIzaSyDZ9fGPHgNx9yGoHa1Xc2oi-Xl5sIP1Umc"
+    // var apiUrl = "https://youtube.googleapis.com/youtube/v3/search?channelId=UCjTzujpWCXoUAe5Nk5tkSFA&key=AIzaSyDZ9fGPHgNx9yGoHa1Xc2oi-Xl5sIP1Umc";
 
+    // make a get request to url
+    fetch(apiUrl).then(function(response) {
+    // request was successful
+        if (response.ok) {
+            response.json().then(function(data) {
+            // displayIssues(data);
+                console.log(apiUrl);
+                var videoare = document.querySelector("#new-video");
+                videoare.src = `https://www.youtube.com/embed/${data.items[4].id.videoId}`;
+                // videoare.src = `https://www.youtube.com/embed/d9uxl_7_nWs`;
+            });
+        } else {
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
+        }  
+    });
+};
+
+getYt();
+//getting what is in local storage
+var savingToStorage = function(){
+    localStorage.getItem(myArr, mySound);
+    
+}
+
+//TO DO: event listener for image
