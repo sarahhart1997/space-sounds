b004c1fa29eecd9a7a4b5146a57b9b45b8b8aa7e
ajmarrocco <ajmarrocco@gmail.com>
2022-01-28 22:38:51 -0500

    adding comments

--- develop/js/script.js
+++ develop/js/script.js
@@ -11,7 +11,7 @@ var arr = [
     "Rover Driving Test"
 ];
 //TO BE ERASED: Calls index in array (replace with button call)
-var myArr = arr[1]
+var myArr = arr[8]
 //TO BE ERASED: Console logs index string of array
 console.log(myArr);
 console.log(arr.length);
@@ -51,31 +51,31 @@ sounds = [
     "rover",
 ]
 //TO BE ERASED: Calls index in array (replace with button call)
-var mySound = sounds[1];
+var mySound = sounds[8];
 console.log(mySound);
 //gets youtube video
 var getYt = function(sound) {
-    // format the github api url
-    // var apiUrl = "https://youtube.googleapis.com/youtube/v3/search?channelId=UCvp2nA5u8vapl0iC_qDH2cA&key=AIzaSyDZ9fGPHgNx9yGoHa1Xc2oi-Xl5sIP1Umc";
+    // format the youtube url
     var apiUrl = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${sound}&channelId=UC_aP7p621ATY_yAa8jMqUVA&key=AIzaSyDZ9fGPHgNx9yGoHa1Xc2oi-Xl5sIP1Umc`;
-    // var apiUrl = "https://youtube.googleapis.com/youtube/v3/search?channelId=UCjTzujpWCXoUAe5Nk5tkSFA&key=AIzaSyDZ9fGPHgNx9yGoHa1Xc2oi-Xl5sIP1Umc";
+    //TO BE ERASED: Console log apiURL variable
     console.log(apiUrl);
     // make a get request to url
     fetch(apiUrl).then(function(response) {
     // request was successful
         if (response.ok) {
             response.json().then(function(data) {
-            // displayIssues(data);
-                console.log(apiUrl);
-                var blah = document.createElement("iframe");
-                console.log(blah);
-                var stores = document.querySelector("h2");
-                blah.setAttribute("id","new-video");
-                // var videoare = document.querySelector("#new-video");
-                // console.log(videoare);
-                blah.src = `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
-                // videoare.src = `https://www.youtube.com/embed/d9uxl_7_nWs`;
-                stores.appendChild(blah);
+                //creates iframe
+                var iframeEl = document.createElement("iframe");
+                //console logs iframe
+                console.log(iframeEl);
+                //query selects h2
+                var h2El = document.querySelector("h2");
+                //creates ID for iframe
+                iframeEl.setAttribute("id","new-video");
+                // gives iframe a source
+                iframeEl.src = `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
+                //appends iframe to H2
+                h2El.appendChild(iframeEl);
             });
         } else {
             // if not successful, redirect to homepage
@@ -87,15 +87,20 @@ var getYt = function(sound) {
 
 //getting what is in local storage
 var loadFromStorage = function(){
+    //gets from local storage
     localStorage.getItem(myArr, mySound);
+    //console logs the value
     console.log(mySound);
+    //calls get YT function with mySound variable
     getYt(mySound);
     
 }
 
 //saving to Storage function (probably will require parameter)
 var savingToStorage = function(){
+    //saves to local storage
     localStorage.setItem(myArr, mySound);
+    //calls load from local storage
     loadFromStorage();
 }
 //calls saving Storage function
