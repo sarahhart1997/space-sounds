fd59f89b2e409cd021c84898032e9f0cbdea316b
ajmarrocco <ajmarrocco@gmail.com>
2022-01-28 21:50:26 -0500

    set item in local storage

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,21 +1,23 @@
 //TO BE UPDATED WITH QUERY SELECTORS: Set up array
-var arr =  ["Saturn in Red",
-            "Juno Gets Ready to Shake It",
-            "The Kepler Spacecraft",
-            "Earth Atmosphere Observations",
-            "Ganymede Trailing Hemisphere",
-            "A Wafer of Titan",
-            "Amateur Radio",
-            "Echo Test",
-            "Rover Driving Test"];
-
+var arr = [
+    "Saturn in Red",
+    "Juno Gets Ready to Shake It",
+    "The Kepler Spacecraft",
+    "Earth Atmosphere Observations",
+    "Ganymede Trailing Hemisphere",
+    "A Wafer of Titan",
+    "Amateur Radio",
+    "Echo Test",
+    "Rover Driving Test"
+];
+//TO BE ERASED: Calls index in array (replace with button call)
 var myArr = arr[8]
-
+//TO BE ERASED: Console logs index string of array
 console.log(myArr);
 console.log(arr.length);
-
+//get Photo function
 var getPhoto = function() {
-    // format the github api url
+    // format the nasa image api url
     var apiUrl = `https://images-api.nasa.gov/search?q=${myArr}`;
 
     // make a get request to url
@@ -23,8 +25,9 @@ var getPhoto = function() {
     // request was successful
         if (response.ok) {
             response.json().then(function(data) {
-            // displayIssues(data);
+                // query selector for image
                 var imagine = document.querySelector("img");
+                //gives the src a value
                 imagine.src = data.collection.items[0].links[0].href;
             });
         } else {
@@ -33,5 +36,26 @@ var getPhoto = function() {
         }  
     });
 };
-
+//calls function getPhoto
 getPhoto();
+//TO BE UPDATED WITH SOUNDS OR VIDEO NAMES: creates sound array
+sounds = [
+    "Sound 1",
+    "Sound 2",
+    "Sound 3",
+    "Sound 4",
+    "Sound 5",
+    "Sound 6",
+    "Sound 7",
+    "Sound 8",
+    "Sound 9",
+]
+//TO BE ERASED: Calls index in array (replace with button call)
+var mySound = sounds[8]
+//
+var savingToStorage = function(){
+    localStorage.setItem(myArr, mySound);
+}
+//calls saving Storage function
+savingToStorage();
+
