922dcf56e07951321ae881086d6432639749d537
ajmarrocco <ajmarrocco@gmail.com>
2022-02-01 21:45:20 -0500

    buttons fixed

--- develop/js/script.js
+++ develop/js/script.js
@@ -152,10 +152,13 @@ var myMedias = function(txt){
 var getPhoto = function(event) {
     //clears container element
     // vesselEl.innerHTML ="";
-    photovesselEl.innerHTML="";
     //gets targets ids of the button clicked
     var buttonClicked = event.target.getAttribute("id");
     console.log(buttonClicked);
+    if(buttonClicked === null){
+        return;
+    }
+    photovesselEl.innerHTML="";
     //gets text of the button by the id
     var buttonText = document.getElementById(buttonClicked).textContent.trim();
     if (document.getElementById(buttonClicked).textContent) {
@@ -193,47 +196,48 @@ var getPhoto = function(event) {
 
 var getYt = function (vid) {
     // format the youtube url
-    // var apiUrl = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${vid}&channelId=UC_aP7p621ATY_yAa8jMqUVA&key=AIzaSyDZ9fGPHgNx9yGoHa1Xc2oi-Xl5sIP1Umc`;
-    // //TO BE ERASED: Console log apiURL variable
-    // console.log(apiUrl);
-    // // make a get request to url
-    // fetch(apiUrl).then(function (response) {
-    //     // request was successful
-    //     if (response.ok) {
-    //         response.json().then(function (data) {
-    //         //creates iframe
-    //         var iframeEl = document.createElement("iframe");
-    //         iframeEl.innerHTML ="";
-    //         //console logs iframe
-    //         console.log(iframeEl);
-    //         //creates ID for iframe
-    //         iframeEl.setAttribute("id", "new-video");
-    //         // gives iframe a source
-    //         iframeEl.src = `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
-    //         //appends iframe to H2
-    //         // videovesselEl.appendChild(iframeEl);
-    //         // vesselEl.appendChild(videovesselEl);
-    //         vesselEl.appendChild(iframeEl);
-    //     });
-    //     } else {
-    //         // if not successful, redirect to homepage
-    //         document.location.replace("./index.html");
-    //     }
-    // });
-    console.log(vid);
-    //creates iframe
-    var iframeEl = document.createElement("iframe");
-    // iframeEl.innerHTML ="";
     videovesselEl.innerHTML="";
-    //console logs iframe
-    console.log(iframeEl);
-    //creates ID for iframe
-    iframeEl.setAttribute("id", "new-video");
-    // gives iframe a source
-    iframeEl.src = `https://www.youtube.com/embed/d9uxl_7_nWs`;
-    // vesselEl.appendChild(iframeEl);
-    videovesselEl.appendChild(iframeEl);
-    vesselEl.appendChild(videovesselEl);
+    var apiUrl = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${vid}&channelId=UC_aP7p621ATY_yAa8jMqUVA&key=AIzaSyDZ9fGPHgNx9yGoHa1Xc2oi-Xl5sIP1Umc`;
+    //TO BE ERASED: Console log apiURL variable
+    console.log(apiUrl);
+    // make a get request to url
+    fetch(apiUrl).then(function (response) {
+        // request was successful
+        if (response.ok) {
+            response.json().then(function (data) {
+            //creates iframe
+            var iframeEl = document.createElement("iframe");
+            iframeEl.innerHTML ="";
+            //console logs iframe
+            console.log(iframeEl);
+            //creates ID for iframe
+            iframeEl.setAttribute("id", "new-video");
+            // gives iframe a source
+            iframeEl.src = `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
+            //appends iframe to H2
+            videovesselEl.appendChild(iframeEl);
+            vesselEl.appendChild(videovesselEl);
+            // vesselEl.appendChild(iframeEl);
+        });
+        } else {
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
+        }
+    });
+    // console.log(vid);
+    // //creates iframe
+    // var iframeEl = document.createElement("iframe");
+    // // iframeEl.innerHTML ="";
+    // videovesselEl.innerHTML="";
+    // //console logs iframe
+    // console.log(iframeEl);
+    // //creates ID for iframe
+    // iframeEl.setAttribute("id", "new-video");
+    // // gives iframe a source
+    // iframeEl.src = `https://www.youtube.com/embed/d9uxl_7_nWs`;
+    // // vesselEl.appendChild(iframeEl);
+    // videovesselEl.appendChild(iframeEl);
+    // vesselEl.appendChild(videovesselEl);
 
 };
 //get audio function
